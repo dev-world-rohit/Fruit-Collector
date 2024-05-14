bf986ad3369d7d7db96e8b9f3f1853bc7afa613a
dev_world_rohit <aniationcreatives@gmail.com>
2024-05-15 00:54:13 +0530

    add mouse interactivity

--- data/scripts/game_break.py
+++ data/scripts/game_break.py
@@ -1,8 +1,37 @@
+import pygame
+
+
 class Break:
-    def __init__(self, x, y, name, color, back_color):
+    def __init__(self, x, y, name, size, color, back_color, text, level):
         self.x = x
         self.y = y
         self.name = name
+        self.size = size
         self.color = color
         self.back_color = back_color
+        self.text = text
+        self.text_x = 0
+        self.text_y = 0
+        self.text_shift(level)
         self.hide = True
+
+    def clicked(self):
+        self.hide = False
+        self.color = self.back_color
+
+
+    def text_shift(self, level):
+        if level == "Easy":
+            self.text_x = 20
+            self.text_y = 15
+        elif level == "Medium":
+            self.text_x = 10
+            self.text_y = 5
+        elif level == "Hard":
+            self.text_x = 7
+            self.text_y = 3
+
+    def display_tile(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.size, self.size))
+        if not self.hide:
+            self.text.display_fonts(screen, self.name, [self.x + self.text_x, self.y + self.text_y])
--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from data.scripts.game_break import Break
 
 pygame.init()
 
-screen_size = [550, 550]
+screen_size = [500, 550]
 screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption("Fruit Collector")
 
 
 # generating the breaks numbers as well as the mines
 def breaks(level, game_rule):
-    number_mines = game_rule[level][0]
-    rows = columns = game_rule[level][1]
+    number_mines = game_rule[level][1]
+    rows = columns = game_rule[level][0]
     game_grid = [[0 for _ in range(columns)] for _ in range(rows)]
 
     i = 0
@@ -57,36 +57,83 @@ def finding_numbers(break_grid):
 
 
 # generating the board assigning the values [x, y, color, size, name]. name can be 1, 2, 3, 4, 5, m, or nothing
-def generate_board(level, game_rule):
+def generate_board(level, game_rule, score_area, colors, texts):
     break_grid = breaks(level, game_rule)
     game_board = finding_numbers(break_grid)
+    rows = columns = game_rule[level][0]
+    game_tiles = [[0 for _ in range(columns)] for _ in range(rows)]
+    tile_size = screen_size[0] / game_rule[level][0]
+    m = 0
+    row = 0
+    color = None
+    back_color = None
+    for i in range(len(game_board)):
+        for j in range(len(game_board)):
+            if j > 0:
+                if m == 0:
+                    color, back_color = "l_g_b", "b_b_d"
+                    m = 1
+                else:
+                    color, back_color = "d_g_b", "b_b_l"
+                    m = 0
+            else:
+                if i % 2 == 0:
+                    color, back_color = "l_g_b", "b_b_d"
+                    m = 1
+                elif i % 2 != 0:
+                    color, back_color = "d_g_b", "b_b_l"
+                    m = 0
+
+            tile = Break(i * tile_size, score_area[3] + j * tile_size, str(game_board[i][j]), tile_size, colors[color],
+                   colors[back_color], texts[str(game_board[i][j])], level)
+            game_tiles[i][j] = tile
+
+    return game_tiles
 
 
 # variables--------------------------------------------------------------#
 clock = pygame.time.Clock()
 fps = 30
 
-game_rule = {"Easy": [10, 10],  # [rows/columns, number of mines]
-             "Medium": [18, 40],
-             "Hard": [25, 99]}
+level = "Hard"
+game_rule = {"Easy": [10, 10, 2],  # [rows/columns, number of mines]
+             "Medium": [20, 40, 1],
+             "Hard": [25, 99, 1]}
 
 score_area = [0, 0, screen_size[0], 50]
 
-level = "Easy"
-generate_board(level, game_rule)
-
 score_board_color = (74, 117, 44)
 light_green_breaks = (191, 225, 125)
-dark_green_breaks = (185, 221, 119)
-break_back_light = (229, 194, 159)
-break_back_dark = (178, 150, 123)
+dark_green_breaks = (143, 190, 100)
+break_back_light = (249, 194, 159)
+break_back_dark = (208, 150, 123)
+colors = {
+    "l_g_b": light_green_breaks,
+    "d_g_b": dark_green_breaks,
+    "b_b_l": break_back_light,
+    "b_b_d": break_back_dark,
+}
 
 black_text = font('large_font.png', (255, 255, 255), 1)
-text_1 = font('large_font.png', (0, 255, 0), 1)
-text_2 = font('large_font.png', (0, 0, 255), 1)
-text_3 = font('large_font.png', (255, 0, 0), 1)
-text_4 = font('large_font.png', (255, 0, 255), 1)
-text_5 = font('large_font.png', (255, 255, 0), 1)
+text_1 = font('large_font.png', (0, 255, 0), game_rule[level][2])
+text_2 = font('large_font.png', (0, 0, 255), game_rule[level][2])
+text_3 = font('large_font.png', (255, 0, 0), game_rule[level][2])
+text_4 = font('large_font.png', (255, 0, 255), game_rule[level][2])
+text_5 = font('large_font.png', (255, 255, 0), game_rule[level][2])
+texts = {
+    "0": text_5,
+    "1": text_1,
+    "2": text_2,
+    "3": text_3,
+    "4": text_4,
+    "5": text_5,
+    "6": text_1,
+    "7": text_2,
+    "8": text_3,
+    "m": text_4
+}
+
+game_tiles = generate_board(level, game_rule, score_area,colors, texts)
 
 # main game loop-----------------------------------------------------------------#
 game = True
@@ -96,10 +143,24 @@ while game:
     pygame.draw.rect(screen, score_board_color, score_area)
     black_text.display_fonts(screen, str(level), [30, 18])
 
+    for tiles in game_tiles:
+        for tile in tiles:
+            tile.display_tile(screen)
+
     # binding the keys------------------------------------------------#
     for event in pygame.event.get():
         if event.type == QUIT:
             game = False
+
+        if event.type == MOUSEBUTTONDOWN:
+            mouse_button = pygame.mouse.get_pressed()
+            location = pygame.mouse.get_pos()
+            tile_size = screen_size[0] / game_rule[level][0]
+            x, y = int(location[0] // tile_size), int((location[1] - 50) // tile_size)
+            if mouse_button[0]:
+                game_tiles[x][y].clicked()
+            if mouse_button[2]:
+                print("right")
 
     pygame.display.update()
     clock.tick(fps)
